010dd501d4bd5c4922787c613fda7117af59a08f
prayag-1771 <prayagsharma2648@gmail.com>
2025-08-31 12:30:15 +0530

    Skeleton login page

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import CardDemo from './components/ui/login_form'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <CardDemo/>
+
+    </>
+  )
+}
+
+export default App
